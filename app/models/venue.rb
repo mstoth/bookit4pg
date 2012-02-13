a488633a74c3ab654dc18d20800df21440961ce9
Michael Toth <michael@virtualpianist.com>
2012-02-12 20:56:57 -0500

    s3

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -2,7 +2,11 @@ class Venue < ActiveRecord::Base
   
   has_attached_file :vpic, :styles => { :large => "640x480", :medium => "300x300>", :thumb => "100x100>" },
   :storage => :s3,
-  :s3_credentials => S3_CREDENTIALS,
+  :bucket => 'bookit4pg',
+  :s3_credentials => {
+    :access_key_id => ENV['AKIAICR6FRQRFATSMPKQ'],
+    :secret_access_key => ENV['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']
+  }
   :path => "/:style/:id/:filename"
   
   geocoded_by :zip
