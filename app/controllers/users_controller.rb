d48ebbcf541c9444fdf5b7c91e38891e36758551
Michael Toth <michael@virtualpianist.com>
2012-02-23 01:21:02 -0500

    stop_notification

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_filter :require_no_user, :only=>[:new, :create, :stop_notification]
+  before_filter :require_no_user, :only=>[:new, :create]
   before_filter :require_user, :only=>[:show, :edit, :update]
   before_filter :requre_mst, :only=>[:delete]
 
